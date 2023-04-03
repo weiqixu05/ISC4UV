8b5d9c35fb652f278e7ce73692dcc6cc91921cba
weiqixu05 <weiqixu2005@icloud.com>
2023-04-02 21:38:48 -0400

    change to test

--- test.java
+++ test.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class test {
 	public static void main (String args[]){
 		//initialize scanner
-		Scanner read = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
         System.out.print(1.0/0.0);
 		}
 	}
