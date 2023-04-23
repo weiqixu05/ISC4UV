3be0561765e2727defa2a901ff40071ab147d326
weiqixu05 <weiqixu2005@icloud.com>
2023-04-23 18:02:13 -0400

    test

--- test.java
+++ test.java
@@ -2,9 +2,11 @@
 import java.util.Scanner;
 
 public class test {
+	static int number=1;
 	public static void main (String args[]){
 		//initialize scanner
+		double number=0.1;
 		Scanner sc = new Scanner(System.in);
-		System.out.println((int) 'a');
+		System.out.println(number);
 		}
 	}
