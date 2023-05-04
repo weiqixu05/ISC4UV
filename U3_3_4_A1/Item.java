594ac0c4c01f5d602a5707d7ae5ea9b0c33ebb41
weiqixu05 <weiqixu2005@icloud.com>
2023-05-03 22:53:45 -0400

    class program

---
+++ U3_3_4_A1/Item.java
@@ -0,0 +1,6 @@
+public class Item {
+    //declare String that will store item name
+    String name;
+    //declare int that will store weight
+    int weight;
+}
---
+++ U3_3_4_A1/U3_3_4_A1.java
@@ -0,0 +1,35 @@
+//input scanner class
+import java.util.Scanner;
+
+public class U3_3_4_A1 {
+    public static void main(String args[]){
+        //initialize scanner
+		Scanner sc = new Scanner(System.in);
+        //ask user how many items they would like to enter
+        System.out.println("How many items do you want to enter? ");
+        //input number
+        int n=sc.nextInt();
+        //initialize item class
+        Item[] item=new Item[n];
+        //start of for loop to input information
+        for(int i=0;i<n;i++){
+            //create item object for ith loop
+            item[i]=new Item();
+            //ask user to input the name of the i+1 item
+            System.out.println("Enter the name of item " + (i+1));
+            //input name
+            sc.nextLine();
+            item[i].name=sc.nextLine();
+            //ask user to input the weight
+            System.out.println("Enter the weight of item " + (i+1));
+            //input weight
+            item[i].weight=sc.nextInt();
+        } //end of for loop
+        //output table header
+        System.out.printf("%-15s%8s\n", "Item", "Weight");
+        //start of for loop to output information
+        for(int i=0;i<n;i++){
+            System.out.printf("%-15s%8s\n", item[i].name, item[i].weight);
+        }
+    }
+}
