66baea5aa518bd5c81b1cba514d66417247f99e9
weiqixu05 <weiqixu2005@icloud.com>
2023-06-19 22:44:44 -0400

    some test code in java

--- test.java
+++ test.java
@@ -4,12 +4,8 @@ import java.util.Scanner;
 public class test {
 	static int number=1;
 	public static void main (String args[]){
-		//initialize scanner
-		double number=0.1;
-		Scanner sc = new Scanner(System.in);
-		int m=1;
-		int b=2;
-		String y="y = " + Integer.toString(m) + "x + " + Integer.toString(b);
-		System.out.println(y);
-		}
+		
+                System.out.println("bob".indexOf('b'));
+            
 	}
+}
